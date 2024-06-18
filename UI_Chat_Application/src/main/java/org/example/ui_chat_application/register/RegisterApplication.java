0dbb96c82c9c2b94c6daafe79a17fa4afd355593
RafifPratama <rafpratama852@gmail.com>
2024-06-18 14:35:56 +0700

    can start login

---
+++ UI_Chat_Application/src/main/java/org/example/ui_chat_application/register/RegisterApplication.java
@@ -0,0 +1,13 @@
+package org.example.ui_chat_application.register;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class RegisterApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+
+    }
+}
---
+++ UI_Chat_Application/src/main/java/org/example/ui_chat_application/register/RegisterController.java
@@ -0,0 +1,4 @@
+package org.example.ui_chat_application.register;
+
+public class RegisterController {
+}
